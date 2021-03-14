68a041f8d2bd63f498afe0929400de04811f8508
Douglas Berdeaux <weaknetlabs@gmail.com>
2021-03-14 14:59:22 -0400

    Create iplistgen.py

---
+++ iplistgen.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# 2021 Douglas Berdeaux
+# Sorts out IP lists/ranges provided by clients
+#   and creates a comma separated list of all expanded ranges/sets.
+#
+from netaddr import IPNetwork
+import sys
+from os import path
+from re import match
+from re import sub
+def usage():
+    print(f"\nUsage: ./iplistgen.py (file containing client's IPs and IP ranges.)\n")
+    sys.exit(1)
+def error(msg):
+    print(f"[!] ERROR: {msg}")
+    sys.exit(1)
+if len(sys.argv)!=2:
+    usage()
+## OK, we got a file to test:
+ip_file=sys.argv[1]
+if path.isfile(ip_file): # File is OK:
+    with open(ip_file,"r") as fh:
+        for line in fh:
+            range=line.rstrip()
+            if(match("^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+$",range)):
+                for ip in IPNetwork(range):
+                    print(f"{ip},",end="")
+            elif(match("^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+-[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$",range)):
+                start_ip=range.split("-")[0]
+                end_ip=range.split("-")[1]
+                ip_123_oct = sub("\.[0-9]+$","",start_ip)
+                start_oct=start_ip.split(".")[3]
+                end_oct=end_ip.split(".")[3]
+                if(int(start_oct)<int(end_oct)):
+                    i = int(start_oct)
+                    while i <= int(end_oct):
+                        print(f"{ip_123_oct}.{i},",end="")
+                        i+=1
+                else:
+                    error("IP Address range might be out of order.")
+            elif(match("^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+-[0-9]+$",range)):
+                start_ip=range.split("-")[0]
+                ip_123_oct = sub("\.[0-9]+$","",start_ip)
+                start_oct=range.split("-")[0].split(".")[3]
+                end_oct=range.split("-")[1]
+                if(int(start_oct)<int(end_oct)):
+                    i = int(start_oct)
+                    while i <= int(end_oct):
+                        print(f"{ip_123_oct}.{i},",end="")
+                        i+=1
+                else:
+                    error("IP Address range might be out of order.")
+            elif(match("^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$",range)):
+                print(f"{range},",end="")
+else:
+    error(f"Could not open file: {ip_file}")
+print("")
